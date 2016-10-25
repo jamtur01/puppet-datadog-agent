313aace51bf07014e51fdd1922461272ccfda437
Ben Ford <ben.ford@puppetlabs.com>
2016-10-25 11:29:20 -0700

    Allow the report processor to run on PE
    The PE puppetserver runs as `pe-puppet`, not `puppet`.
    
    The $settings::user variable resolves to the user that the Puppet Master node is configured to run as. This patch assumes that you are either enforcing as the agent on the Puppet Master node, or that if you're enforcing on a compile master that it will be the same as the Master of Masters.

--- manifests/init.pp
+++ manifests/init.pp
@@ -26,7 +26,7 @@
 class datadog(
   $api_key = 'your key',
   $puppet_run_reports = false,
-  $puppetmaster_user = 'puppet'
+  $puppetmaster_user = $settings::user,
 ) inherits datadog::params {
 
   include datadog::params
